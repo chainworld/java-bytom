34b9274fa4e65fdec00bde1e4bb70a4c4e178a51
niyuelin1990 <niyuelin1990@163.com>
2018-05-22 18:49:20 +0800

    update blocktx

--- src/main/java/io/bytom/api/BlockTx.java
+++ src/main/java/io/bytom/api/BlockTx.java
@@ -13,93 +13,38 @@ public class BlockTx {
 	 /**
      * Unique identifier, or transaction hash, of a transaction.
      */
-    private String id;
+    public String id;
 
     /**
      * version
      */
-    private long version;
+    public long version;
     /**
      * size
      */
-    private long size;
+    public long size;
     /**
      * time_range
      */
     @SerializedName("time_range")
-    private long timeRange;
+    public long timeRange;
 
     /**
      * status
      */
     @SerializedName("status_fail")
-    private boolean statusFail;
+    public boolean statusFail;
 
     /**
      * List of specified inputs for a transaction.
      */
-    private List<Transaction.Input> inputs;
+    public List<Transaction.Input> inputs;
 
     /**
      * List of specified outputs for a transaction.
      */
-    private List<Transaction.Output> outputs;
+    public List<Transaction.Output> outputs;
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public long getVersion() {
-		return version;
-	}
-
-	public void setVersion(long version) {
-		this.version = version;
-	}
-
-	public long getSize() {
-		return size;
-	}
-
-	public void setSize(long size) {
-		this.size = size;
-	}
-
-	public long getTimeRange() {
-		return timeRange;
-	}
-
-	public void setTimeRange(long timeRange) {
-		this.timeRange = timeRange;
-	}
-
-	public boolean isStatusFail() {
-		return statusFail;
-	}
-
-	public void setStatusFail(boolean statusFail) {
-		this.statusFail = statusFail;
-	}
-
-	public List<Transaction.Input> getInputs() {
-		return inputs;
-	}
-
-	public void setInputs(List<Transaction.Input> inputs) {
-		this.inputs = inputs;
-	}
-
-	public List<Transaction.Output> getOutputs() {
-		return outputs;
-	}
-
-	public void setOutputs(List<Transaction.Output> outputs) {
-		this.outputs = outputs;
-	}
 
 	@Override
 	public String toString() {
